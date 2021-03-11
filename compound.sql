f4123bf3f2a8ecb78bf3a2d8c1bfe1951367855a
paulapivat <paul.apivat@gmail.com>
2021-03-12 00:39:50 +0700

    add queries for compound

--- compound.sql
+++ compound.sql
@@ -1,6 +1,31 @@
 /* Compound is a Lending & Borrowing Platform */
 
 /* See what people borrowed on Compound in 2020 */
+/* TIME RANGE */
 SELECT * FROM compound.view_borrow
 WHERE block_time >= '2020-01-01 00:00' AND block_time < '2021-01-01 00:00'
 LIMIT 20
+
+
+/* Count Number of Token Symbol */
+SELECT COUNT(DISTINCT(token_symbol)) FROM compound.view_borrow
+WHERE block_time >= '2020-01-01 00:00' AND block_time < '2021-01-01 00:00'
+LIMIT 5
+
+/* How much people borrowed Tokens on Compound in 2020 */
+SELECT token_symbol, total_borrows_usd FROM compound.view_borrow
+WHERE block_time >= '2020-01-01 00:00' AND block_time < '2021-01-01 00:00'
+LIMIT 20
+
+/* How to do GROUP BY */
+
+/* Compound View Accrue Interest */
+
+/* Filter by token: DAI - LIMIT can vary */
+SELECT * FROM compound."view_accrue_interest"
+WHERE block_time >= '2020-01-01 00:00' 
+AND block_time < '2021-01-01 00:00' 
+AND token_symbol = 'DAI'
+LIMIT 18
+
+/* Comp Unique Borrowers Per Day */
--- initial_queries.sql
+++ initial_queries.sql
@@ -18,8 +18,8 @@ select * from ethereum."transactions" order by block_time desc limit 15
 
 /* sample query 2 */
 
-# NOTE: ETH has 18 decimals precision, divide number by 1e18 to get values in ETH not in Wei
-# Summarizing total value ETH sent in the last 10 days (also change to 30 days)
+/* NOTE: ETH has 18 decimals precision, divide number by 1e18 to get values in ETH not in Wei */
+/* Summarizing total value ETH sent in the last 10 days (also change to 30 days) */
 
 SELECT date_trunc('day', block_time) AS "Date", sum(value/1e18) AS "Value" 
 FROM ethereum."transactions" 
