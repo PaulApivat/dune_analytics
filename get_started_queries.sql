3498f9a01be0434f04f3e5b18804605e143eadb9
paulapivat <paul.apivat@gmail.com>
2021-03-12 11:53:10 +0700

    add all assets bought on uniswap 24 hrs

--- get_started_queries.sql
+++ get_started_queries.sql
@@ -37,13 +37,64 @@ ORDER BY hour DESC
 
 /* WHERE ELSE (WHICH OTHER DEXES) WAS DAI BOUGHT - ASIDE FROM UNISWAP */
 /* Expected Ans: 0x API 0x Native, 1inch, Balancer, Curve, Kyber, Sushiswap etc */
-SELECT SUM(token_a_amount) AS dai_bought, project
+SELECT 
+    SUM(token_a_amount) AS dai_bought, 
+    project
 FROM dex."trades"
 WHERE block_time > now() - interval '24 hours'    
 AND token_a_symbol = 'DAI'                        
 GROUP BY project
 
+/* Makes no difference to use dex."trades" t and t.project */
+SELECT 
+    SUM(token_a_amount) AS dai_bought, 
+    project
+FROM dex."trades" t
+WHERE block_time > now() - interval '24 hours'    
+AND token_a_symbol = 'DAI'                        
+GROUP BY t.project
+
+/* WHERE WAS WBTC BOUGHT OVER 24 HRS */ 
+/* Bar Chart or Pie Chart */
+SELECT 
+    SUM(token_a_amount) AS wbtc_bought,
+    project
+FROM dex."trades" t
+WHERE block_time > now() - interval '24 hours'    
+AND token_a_symbol = 'WBTC'
+GROUP BY t.project
+
+/* All Token Volume on DEX vs DEX Aggregator */
+/* What's a DEX Aggregator */
+SELECT 
+    SUM(token_a_amount) as amount, 
+    category 
+FROM dex."trades" 
+WHERE block_time > now() - interval '24 hours'  
+GROUP BY category
+
+/* Total Token Volume across Projects */
+SELECT 
+    SUM(token_a_amount) as amount,    /* can change to token_b_amount */
+    project 
+FROM dex."trades" 
+WHERE block_time > now() - interval '24 hours'  
+GROUP BY project
+
+
 /* ALL ASSETS (Including DAI) BOUGHT ON UNISWAP in last 24 HRS */
+/* SUM AMOUNT, accompanied by GROUP BY symbol, project */
+SELECT 
+    token_a_symbol, 
+    SUM(token_a_amount) as amount, 
+    project
+FROM dex."trades" 
+WHERE block_time > now() - interval '24 hours' 
+AND project = 'Uniswap'
+GROUP BY token_a_symbol, project
+
+
+
 
 /* select relevant columns */
 SELECT 
